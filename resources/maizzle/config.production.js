345937ba4f42ceb3832213b56844a53e09e68233
Vyacheslav Razum <modern.web.artisan@gmail.com>
2020-11-07 01:17:11 +0200

    Maizzle installed

---
+++ resources/maizzle/config.production.js
@@ -0,0 +1,19 @@
+module.exports = {
+  build: {
+    destination: {
+      path: 'build_production',
+    },
+  },
+
+  inlineCSS: {
+    enabled: true,
+  },
+
+  removeUnusedCSS: {
+    enabled: true,
+  },
+
+  prettify: {
+    enabled: true,
+  },
+}
